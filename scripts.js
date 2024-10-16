6b8afb338ab99607a9b8f30dbcb394f25ea061df
Ethanyns <58254541+Ethanyns@users.noreply.github.com>
2024-10-16 12:18:43 +0100

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,4 @@
+document.getElementById('action-button').addEventListener('click', () => {
+    alert('Button clicked!');
+    // Here you can add more functionality, like sending data to your server or interacting with the Telegram API
+});
